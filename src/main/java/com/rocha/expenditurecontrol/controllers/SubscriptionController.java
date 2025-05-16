468579067d85a019dc3106143d28f2399880712d
erickrocha87 <ericksaggi31@gmail.com>
2025-05-16 11:16:18 -0300

    feat: implements filter with status column

--- src/main/java/com/rocha/expenditurecontrol/controllers/SubscriptionController.java
+++ src/main/java/com/rocha/expenditurecontrol/controllers/SubscriptionController.java
@@ -3,15 +3,12 @@ package com.rocha.expenditurecontrol.controllers;
 import com.rocha.expenditurecontrol.dtos.PageResponseDTO;
 import com.rocha.expenditurecontrol.dtos.SubscriptionRequestDTO;
 import com.rocha.expenditurecontrol.dtos.SubscriptionResponseDTO;
-import com.rocha.expenditurecontrol.entities.Subscription;
 import com.rocha.expenditurecontrol.services.SubscriptionService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/subscriptions")
@@ -23,11 +20,22 @@ public class SubscriptionController {
     @PostMapping
     public ResponseEntity<SubscriptionResponseDTO> createSubscription(@Valid @RequestBody SubscriptionRequestDTO subscription) {
         SubscriptionResponseDTO subs = subscriptionService.createSubscription(subscription);
+        if (subs == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
         return ResponseEntity.status(HttpStatus.CREATED).body(subs);
     }
 
     @GetMapping
-    public ResponseEntity<PageResponseDTO<SubscriptionResponseDTO>> getAllSubscriptions(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size ) {
+    public ResponseEntity<PageResponseDTO<SubscriptionResponseDTO>> getAllSubscriptions(@RequestParam(required = false) String status, @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "7") int size) {
+        System.out.println(status);
+        if (status != null) {
+            if (!status.equals("ALL")) {
+            PageResponseDTO<SubscriptionResponseDTO> subsFiltered = subscriptionService.getByStatus(status, page, size);
+            System.out.println(subsFiltered);
+            return ResponseEntity.ok().body(subsFiltered);
+            }
+        }
         PageResponseDTO<SubscriptionResponseDTO> subs = subscriptionService.getUserSubscriptions(page, size);
         return ResponseEntity.ok().body(subs);
     }
@@ -38,8 +46,8 @@ public class SubscriptionController {
     }
 
     @PutMapping("/{id}")
-        public ResponseEntity<Subscription> updateSubscription(@PathVariable Long id, @Valid @RequestBody Subscription subscription) {
-        Subscription subs = subscriptionService.updateSubscription(id, subscription);
+    public ResponseEntity<SubscriptionResponseDTO> updateSubscription(@PathVariable Long id, @Valid @RequestBody SubscriptionRequestDTO subscription) {
+        SubscriptionResponseDTO subs = subscriptionService.updateSubscription(id, subscription);
         return ResponseEntity.ok().body(subs);
-    } //tirar a entidade da controller
+    }
 }
