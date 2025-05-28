215352bd968b9a321601f553c0b6d8c524505743
erickrocha87 <ericksaggi31@gmail.com>
2025-05-28 13:59:03 -0300

    refactor: implements interface responsible only for documentation

--- src/main/java/com/rocha/expenditurecontrol/controllers/SubscriptionController.java
+++ src/main/java/com/rocha/expenditurecontrol/controllers/SubscriptionController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/subscriptions")
 @RequiredArgsConstructor
-public class SubscriptionController {
+public class SubscriptionController implements SubscriptionControllerDoc {
 
     private final SubscriptionService subscriptionService;
 
